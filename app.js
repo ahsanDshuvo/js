a56a8f2dec55e9097a67623e5b49c44fae3eec9e
ahsanDshuvo <101447862+ahsanDshuvo@users.noreply.github.com>
2022-07-21 22:22:27 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,96 @@
+// var name = 'shuvo';
+// if (name.length > 4){
+//     name = 'ahsan d shuvo';
+// }
+// console.log (name);
+
+// function CheckLeapYear(year) {
+    
+//     if((0 == year % 4) && ( 0 != year %100) || ( 0 == year % 400)){
+//         console.log('this is a leap year');
+//     }
+//     else{
+//         console.log('not a leap year');
+//     }
+// } 
+//      const year = prompt ('enter a year : 564');
+
+// var p = 5;
+// var q = 7;
+// [p,q] = [q,p];
+// console.log('after the swap ',' p =', p,'q=', q);
+// var apple = 666;
+// var mango = 444;
+// var orange = 777;
+
+// var max = Math.max (apple, mango , orange )
+// console.log(max);
+
+// function FeetToMile (feet){
+//     var mile = feet / 5280 ;
+//     return mile;
+// }
+// var nana = FeetToMile(73657834);
+
+// var result = Math.ceil(nana)
+// console.log(result );
+
+// var marks = [11, 5345, 4535, 35345, 345345, 32543245, 'abir', 'babir', 'mabir', 'chabir'];
+// for (let i = 0; i < marks.length; i++) {
+//     const element = marks[i];
+//     console.log(element);
+// }
+// function Fuck(number1,number2) {
+//     var total = number1 + number2;
+//     return total;
+// }
+// var mew = Fuck(66,88);
+// console.log(mew);
+// console.log('fuck off');
+// console.log('fuck you too');
+// var FuckOff = Fuck(77,88);
+// console.log(FuckOff);
+// console.log('mew');
+
+// function Java(num1,num2) {
+
+//     var total = num1 + num2;
+//     return total;
+    
+// }
+// var mew = Java(66,88);
+// console.log(mew);
+// var aww = Java(43567,734986);
+// console.log(aww);
+// var Love = Java('love ','you');
+// console.log(Love);
+// 
+    //     var total = name1 + name2;
+    //     return total;
+    // }
+    // var result1 = name(66 , 88);
+    // console.log(result1);
+    // console.log("fuck off");
+     
+    // var result2 = name(99 , 7349);
+    // console.log(result2);
+    // console.log("highest number"
+    // );
+    
+    // function gun(num1 , num2) {
+    //     var aksathe = num1 * num2;
+    //     return aksathe;
+    
+    // }
+    // var prothom = gun(66 , 88);
+    // console.log(prothom);
+    // console.log("eta holo prothom gun");
+    
+    // var second = gun(99 , 88);
+    // console.log(second);
+    // console.log("eta holo ditiyo");
+// var numbers = [55, 66, 4564, 3463, 35634, 363, 3636, 3623, 141 ];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     console.log(element);
+//
